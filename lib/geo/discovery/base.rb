36818fbc19002e05f3a7cc431bb2e22d1ac0bf9a
Hendrik Bergunde <hbergunde@gmx.de>
2015-06-05 20:10:37 +0200

    provide json compat in Geo::Discovery::Base

--- lib/geo/discovery/base.rb
+++ lib/geo/discovery/base.rb
@@ -7,6 +7,17 @@ module Geo
     # source, talking about a certain representation of time
     class Base
       attr_value_initialize :source, :time
+
+      def to_hash
+        {
+          source: source,
+          time: time
+        }
+      end
+
+      def to_json(state = nil)
+        to_hash.to_json(state)
+      end
     end
   end
 end
